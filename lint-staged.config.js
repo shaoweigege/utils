1092b643f3a205c9485a3272b165a5f0c027c9ff
fatesigner <fatesigner@gmail.com>
2019-07-20 22:19:37 +0800

    initial: initial commit

---
+++ lint-staged.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'build/**/*.{js,jsx}': [
+    'eslint --fix --cache --quiet',
+    'prettier --write --ignore-path .eslintignore'
+  ],
+  'src/**/*.{js,jsx}': [
+    'eslint --fix --cache --quiet',
+    'prettier --write --ignore-path .eslintignore'
+  ],
+  'src/**/*.{ts,tsx}': [
+    'eslint --fix --cache --quiet',
+    'prettier --write --ignore-path .eslintignore --parser typescript'
+  ],
+  'src/**/*.{css,scss}': ['stylelint --fix --cache --quiet']
+};
---
+++ src/typings/window.d.ts
@@ -0,0 +1,27 @@
+/**
+ * window.d.ts
+ */
+
+interface Window {
+  ActiveXObject: any;
+  navigator: any;
+  webkitURL: any;
+  imagePicker: any;
+  plugins: any;
+  DocumentTouch: any;
+  resolveLocalFileSystemURL: any;
+  requestIdleCallback: any;
+  external: any;
+}
+
+interface HTMLElement {
+  createTextRange: () => any;
+}
+
+declare let require: any;
+
+declare let window: Window;
+
+declare let DocumentTouch: Window.DocumentTouch;
+
+declare let navigator: Window.navigator;
